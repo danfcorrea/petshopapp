6360db2a543b22154b7e64c98f18669c5653d374
Daniel Corrêa <daniel.1997.df@gmail.com>
2024-04-09 11:49:25 -0300

    feat: initial commit

---
+++ src/main/java/com/petshopapp/PetshopappApplication.java
@@ -0,0 +1,13 @@
+package com.petshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetshopappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petshopapp/PetshopappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
