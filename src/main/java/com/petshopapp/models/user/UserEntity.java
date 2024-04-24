1dc5008cf773e2d9f6c5a97c38d27e836d53ce50
correamarcos <83926140+correamarcos@users.noreply.github.com>
2024-04-24 16:51:35 -0300

    feat: adicionados atributos e relacionamentos na classe user

--- src/main/java/com/petshopapp/models/user/UserEntity.java
+++ src/main/java/com/petshopapp/models/user/UserEntity.java
@@ -1,4 +1,36 @@
 package com.petshopapp.models.user;
 
+import com.petshopapp.models.common.DispositivoEntity;
+import com.petshopapp.models.common.EnderecoEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "USER")
+@Table(name = "USER")
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String cpfCnpj;
+    private Date dtNascimento;
+    private String telefone;
+    private String email;
+    private String senha;
+    private Date dtCadastro;
+    private Date dtUltimoAcesso;
+    @OneToMany(mappedBy = "user")
+    private List<EnderecoEntity> endereco;
+    @OneToMany(mappedBy = "user")
+    private List<DispositivoEntity> dispositivos;
 }
