5eb9d9fca2d50364bb1638297cb4b3fb462d6b7b
correamarcos <83926140+correamarcos@users.noreply.github.com>
2024-04-27 14:25:18 -0300

    feat: implementado spring security

---
+++ src/main/java/com/petshopapp/infra/security/TokenService.java
@@ -0,0 +1,46 @@
+package com.petshopapp.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.petshopapp.models.user.UserEntity;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+    public String generateToken(UserEntity user){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("petshop-app")
+                    .withSubject(user.getEmail())
+                    .withSubject(user.getCpfCnpj())
+                    .withExpiresAt(this.generateExpirationToken())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Error while authenticating.");
+        }
+    }
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("petshop-app")
+                    .build().verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException e){
+            return null;
+        }
+    }
+    private Instant generateExpirationToken(){
+        return LocalDateTime.now().plusHours(24).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
