f7b50a4fd1c0e4c9ba05a759833fab6f873c98d0
correamarcos <83926140+correamarcos@users.noreply.github.com>
2024-04-29 08:14:35 -0300

    fix: update security, acesso h2

--- src/main/java/com/petshopapp/infra/security/SecurityConfig.java
+++ src/main/java/com/petshopapp/infra/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.petshopapp.infra.security;
 
+import jakarta.servlet.DispatcherType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,9 +31,11 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+        http.headers(headers -> headers.frameOptions(frame -> frame.sameOrigin()));
         return http.build();
     }
     @Bean
